0d28b724167fc91261a8ddade498f0105cdce3b9
Juan Sebastian P <100741303+juxse@users.noreply.github.com>
2023-12-03 10:51:18 +0700

    Import

---
+++ script.js
@@ -0,0 +1,7 @@
+function showProductDetail(productId) {
+    document.getElementById(productId + 'Detail').style.display = 'flex';
+}
+
+function hideProductDetail(productDetailId) {
+    document.getElementById(productDetailId).style.display = 'none';
+}
